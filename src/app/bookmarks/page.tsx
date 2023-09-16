7a2a91a2720387c49b92979a1fba9b9fddff5abb
Abdirahman Haji <abdirahman.haji.13@gmail.com>
2023-09-16 09:09:07 -0500

    fix build errors

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -4,6 +4,8 @@ import { redirect } from 'next/navigation';
 import SignOut from '@/components/auth/SignOut';
 import Image from 'next/image';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Bookmarks() {
   const supabase = createServerComponentClient({
     cookies,
